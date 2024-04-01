947c9874ab02cff49ea3703059c26b8c0bb2edf5
Jan Kubíček <kubicja1@uhk.cz>
2024-04-01 16:00:13 +0200

    next

--- src/main/java/org/example/pro2projekt/controller/dataInput.java
+++ src/main/java/org/example/pro2projekt/controller/dataInput.java
@@ -57,13 +57,16 @@ public class dataInput {
     }
 
     public ResultSet getLets(String stat1, String stat2){
-        ResultSet set = database.getData("SELECT * FROM Let L\n" +
+        ResultSet set = database.getData("SELECT L.LetID AS LetId, L.LetadloID AS LetadloID, L.Cas_odletu AS Cas_Odletu, L.Cas_priletu AS Cas_Priletu,\n" +
+                "LT1.Mesto AS MestoOdletu, LT1.Stat AS StatOdletu, LT1.Nazev AS NazevLOdletu, LT2.Mesto AS MestoPriletu,\n" +
+                "LT2.Stat AS StatPriletu, LT2.Nazev AS NazevLPriletu\n" +
+                " FROM Let L\n" +
                 "JOIN Smerletu S ON L.SmerletuID = S.SmerletuID\n" +
                 "JOIN letistesmerletu LSL ON S.SmerletuID = LSL.SmerletuID\n" +
                 "JOIN letistesmerletu LSL1 ON S.SmerletuID = LSL1.SmerletuID\n" +
                 "JOIN Letiste LT1 ON LSL.LetisteID = LT1.LetisteID\n" +
                 "JOIN Letiste LT2 ON LSL1.LetisteID = LT2.LetisteID\n" +
-                "WHERE (LSL.LetisteID != LSL1.LetisteID ) AND (LT1.Stat = "+stat1+" + AND LT2.Stat = "+stat2+")");
+                "WHERE (LSL.LetisteID != LSL1.LetisteID ) AND (LT1.Stat = '"+stat1+"' AND LT2.Stat = '"+stat2+"')");
         return set;
     }
 }
--- src/main/java/org/example/pro2projekt/views/index.java
+++ src/main/java/org/example/pro2projekt/views/index.java
@@ -1,6 +1,7 @@
 package org.example.pro2projekt.views;
 
 import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Paragraph;
 import org.example.pro2projekt.controller.dataInput;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -14,24 +15,42 @@ import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLink;
 
 import java.awt.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @PageTitle("index")
 @Route("/")
-public class index extends VerticalLayout  {
+public class index extends VerticalLayout {
     Button btnLogin, btnRegister,btnSubmit;
     RouterLink lingLogin, likRegister;//todo
+    String statOdletu = "";
+    String statPriletu = "";
     dataInput input = new dataInput();
+    ComboBox<String> odletField, priletField;
+    Div lets;
     public index(){
         //componenty
         btnLogin = new Button("Login");
         btnRegister = new Button("Register");
         btnSubmit = new Button("Submit");
+
         //header
         add(importZahlavi());
         //nav
 
         //main
         add(importMain());
+
+        btnSubmit.addClickListener(event -> {
+            remove(lets);
+            lets = new Div();
+            lets = importLety(input.getLets(statOdletu, statPriletu));
+            add(lets);
+        });
+
+        odletField.addValueChangeListener(event -> {statOdletu = event.getValue(); });
+        priletField.addValueChangeListener(event -> {statPriletu = event.getValue(); });
+
         //footer
 
         //
@@ -40,6 +59,48 @@ public class index extends VerticalLayout  {
         setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);
     }
+    private void refreshLety() {
+        removeAll();
+        add(importZahlavi());
+        add(importMain());
+        add(importLety(input.getLets(statOdletu, statPriletu)));
+    }
+    private Div importLety(ResultSet resultSet){
+    // LetId LetadloID Cas_Odletu Cas_Priletu MestoOdletu StatOdletu NazevLOdletu MestoPriletu StatPriletu NazevLPriletu
+        Div div = new Div();
+
+        try {
+            while (resultSet.next()) {
+                // Získání hodnot z ResultSet pro každý řádek
+                int letId = resultSet.getInt("LetId");
+                int letadloId = resultSet.getInt("LetadloID");
+                String casOdletu = resultSet.getString("Cas_Odletu");
+                String casPriletu = resultSet.getString("Cas_Priletu");
+                String mestoOdletu = resultSet.getString("MestoOdletu");
+                String statOdletu = resultSet.getString("StatOdletu");
+                String nazevLOdletu = resultSet.getString("NazevLOdletu");
+                String mestoPriletu = resultSet.getString("MestoPriletu");
+                String statPriletu = resultSet.getString("StatPriletu");
+                String nazevLPriletu = resultSet.getString("NazevLPriletu");
+
+                // Vytvoření textového řetězce s daty
+                String rowData = "Let ID: " + letId + ", Letadlo ID: " + letadloId + ", Čas odletu: " + casOdletu +
+                        ", Čas příletu: " + casPriletu + ", Město odletu: " + mestoOdletu +
+                        ", Stát odletu: " + statOdletu + ", Název letiště odletu: " + nazevLOdletu +
+                        ", Město příletu: " + mestoPriletu + ", Stát příletu: " + statPriletu +
+                        ", Název letiště příletu: " + nazevLPriletu;
+
+                // Vytvoření paragraph elementu pro každý řádek dat
+                Paragraph paragraph = new Paragraph(rowData);
+                div.add(paragraph);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace(); // Pokud dojde k chybě při čtení z ResultSet
+        }
+
+        return div;
+    }
+
 
     private Div importZahlavi(){
         Div zahlaviDiv = new Div();
@@ -68,12 +129,12 @@ public class index extends VerticalLayout  {
         main.setMaxWidth("80%");
 
         Text odlet = new Text("Stát odeltu: ");
-        ComboBox<String> odletField = new ComboBox<>();
+         odletField = new ComboBox<>();
         odletField.setItems(input.getAllStates());
         odletField.setPlaceholder("Vyberte stát odletu");
 
         Text prilet = new Text("Stát příletu: ");
-        ComboBox<String> priletField = new ComboBox<>();
+        priletField = new ComboBox<>();
         priletField.setItems(input.getAllStates());
         priletField.setPlaceholder("Vyberte stát příletu");
 
@@ -105,12 +166,8 @@ public class index extends VerticalLayout  {
         Div row6 = new Div(btnSubmit);
         btnSubmit.setWidthFull();
 
-
         main.add(row1,row2,row3,row4,row5,row6);
-
 
         return main;
     }
-
-
 }
